62645409942a33b60299f219129720a0ebb07561
Chris Diab <chrisdiab710@gmail.com>
2023-12-21 11:45:49 -0500

    second commit with hash

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //we need to call our API first
-//const API = "https://fsa-puppy-bowl.herokuapp.com/api/2309-ftb-et-am/players "
+const API = "https://fsa-puppy-bowl.herokuapp.com/api/2309-ftb-et-am/players "
 
 //for review: what is state and why is it important 
 //state is what is changing - what is the DATA and when do I get it
@@ -13,19 +13,46 @@ let puppies = [];
 const list = document.querySelector(".puppies");
 console.log(list);
 
+window.addEventListener('hashchange', function(){
+    //console.log('change');
+    render();
+});
+
+
 function render(){
     
     span.innerHTML = puppies.length;
-    const html = puppies.map(function(puppy){
+    const hash = window.location.hash;
+    const id = hash.slice(1)*1;
+    let filtered = puppies;
+    if(id){
+        filtered = filtered.filter(function(puppy){
+            return puppy.id === id;
+
+        });
+        console.log(id);
+    }
+    const html = filtered.map(function(puppy){
        return `
-        <li>
+        <h2><a href = "#${puppy.id}">
         ${puppy.name}
+        </a>
+        </h2>
+        <li>
+        ${puppy.breed}
         </li>
+        <em>
+        ${puppy.status}
+        </em>
+        <img src = "${puppy.imageUrl}" />
+    
        `;
     }).join('');
     list.innerHTML = html;
    
 }
+
+//Get ur images in on the render function!!!  ??
 
 render();
 
@@ -42,4 +69,5 @@ fetchPuppies();
 
 
 
-  //what exactly does try and catch do?
+//what exactly does try and catch do?
+//file:///C:/Users/robed/Coursework/career%20simulation/app.html#undefined
